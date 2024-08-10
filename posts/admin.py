abd4e9a3fe5356355e3ae6cfc42c3f1ca2c4249b
Usynin Sergei <usynin.s00@mail.ru>
2024-08-10 17:49:20 +0300

    Add verbose_names.

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import News, Review, Artist, Tag
 
 # Register your models here.
+admin.site.register(News)
+admin.site.register(Review)
+admin.site.register(Artist)
+admin.site.register(Tag)
--- posts/models.py
+++ posts/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
 class News(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+
     ### добавить images и еще что-то
+
+    class Meta:
+        verbose_name_plural = 'Новости'
+        verbose_name = 'Новость'
 
 
 class Review(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     artist = models.ForeignKey("Artist", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="images/", null=True, blank=True)
+
     # добавить images(желательно несколкьо) тип картины Барокко и тд, год создания можно, и еще какие-либо данные
+
+    class Meta:
+        verbose_name_plural = 'Обзоры картин'
+        verbose_name = 'Обзор картин'
 
 
 class Artist(models.Model):
@@ -19,7 +32,15 @@ class Artist(models.Model):
     country = models.CharField(max_length=120)
     ###  возможно еще стиль или эпоху
 
+    class Meta:
+        verbose_name_plural = 'Художники'
+        verbose_name = 'Художник'
 
-class Tags(models.Model):
+
+class Tag(models.Model):
     name = models.CharField(max_length=120)
     ### связь с новостями или типами продумать
+
+    class Meta:
+        verbose_name_plural = 'Тэги'
+        verbose_name = 'Тэг'
