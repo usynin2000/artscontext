e172eaca312cc4acca15a04c1f9a05e5ba5b9162
Usynin Sergei <usynin.s00@mail.ru>
2024-08-12 23:01:08 +0300

    Add small post template.

--- core/urls.py
+++ core/urls.py
@@ -19,8 +19,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from posts.views import home_view
+from posts.views import home_view, post_page_view
 
-urlpatterns = [path("admin/", admin.site.urls), path("", home_view, name="home")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", home_view, name="home"),
+    path("post/", post_page_view, name="post-page")
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -9,3 +9,7 @@ def home_view(request):
     news_list = News.objects.all()
 
     return render(request, "index.html", context={"news_list": news_list})
+
+
+def post_page_view(request):
+    return render(request, "post_page.html")
