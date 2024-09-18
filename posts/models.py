6a5050bf6f7ddd2fe0f711386ce282091e461eda
Usynin Sergei <usynin.s00@mail.ru>
2024-09-18 19:04:24 +0300

    Add alt to images.

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
 
-# Create your models here.
-
 
 class News(models.Model):
     title = models.CharField(max_length=120)
